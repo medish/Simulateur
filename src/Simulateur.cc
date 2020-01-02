0bbbcc513ea62bc5f5cf0efe7401bf64e18c19d7
Mohamed Ramdane DEBIANE <mohamed.debiane@ens.uvsq.fr>
2020-01-02 15:44:54 +0100

    XML Parser

--- src/Simulateur.cc
+++ src/Simulateur.cc
@@ -5,13 +5,19 @@
 #include "../include/states/StateManager.h"
 #include "../include/states/MainGState.h"
 #include "../include/states/Loginstate.h"
+#include "../include/utils/xmlparser.h"
 
 int main(int argc, char  *argv[]){	
-
-    QApplication app(argc, argv);
+  /*
+  if(argc > 2){
+       std::cerr << "Erreure d'utilistation: ./Simulateur [Fichier XML] " << std::endl;
+    }
+    */
+   QApplication app(argc, argv);
+   xmlparser parser("../../assets/pannes/pannes1.xml");
+   parser.parseXmlFile();
     StateManager sm;
     Loginstate* ls = new Loginstate();
-    std::cout << "Before Push" << ls << std::endl;
     sm.PushState(ls);
     sm.GetCurrentState()->update();
 
---
+++ src/utils/xmlparser.cc
@@ -0,0 +1,38 @@
+#include "../../include/utils/xmlparser.h"
+
+
+xmlparser::xmlparser(){
+
+}
+
+xmlparser::xmlparser(const char* filepath){
+    file = new QFile(filepath);
+    if(!file->open(QIODevice::ReadOnly)){
+         QMessageBox::warning(this, "Erreur à l'ouverture du document XML", "Le document XML n'a pas pu être ouvert.");
+    }
+    dom = new QDomDocument();
+    //Récuperation des éléments du fichiers XML
+    if(!dom->setContent(file)){
+         file->close();
+         QMessageBox::warning(this, "Erreur à la lecture du document XML", "Le document XML n'a pas pu être lu.");
+    }
+}
+
+xmlparser::~xmlparser(){
+    file->close();
+    delete file;
+}
+
+void xmlparser::parseXmlFile(){
+      QDomElement docElem = dom->documentElement() ;
+      QDomNode n = docElem.firstChild();
+      std::cout << docElem.attributeNode("consomation").value().toStdString() << std::endl;
+      QDomNode panne = docElem.firstChild().firstChild();
+      std::cout <<panne.attributes().namedItem("id").toAttr().value().toStdString()<< std::endl;
+      while(!n.isNull())
+      {
+          QDomElement e = n.toElement();
+          // C'est ici que l'on va marquer le code
+          n = n.nextSibling();
+      }
+}
