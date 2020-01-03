488827f9e0de0b81e350ab9094eb9a8b240982b5
LARBI Mahdi <mahdi.larbi@ens.uvsq.fr>
2020-01-03 21:23:54 +0100

    navbar

--- include/gui/NavBarWidget.h
+++ include/gui/NavBarWidget.h
@@ -4,10 +4,14 @@
 #include <QWidget>
 #include <QVBoxLayout>
 #include <QPushButton>
+#include <QFormLayout>
+#include <QLabel>
 #include "MyQWidget.h"
 class NavBarWidget : public QWidget, public MyQWidget{
 private:
     QVBoxLayout v_layout;
+    QFormLayout f_layout;
+    QLabel label_conso, label_cap, label_time;
     QPushButton btn1, btn2;
 public:
     NavBarWidget();
--- src/gui/NavBarWidget.cc
+++ src/gui/NavBarWidget.cc
@@ -9,6 +9,11 @@ NavBarWidget::NavBarWidget(): btn1("Button1"), btn2("Button2"){
     setMinimumWidth(200);
     v_layout.setAlignment(Qt::AlignTop);
 
+    f_layout.addRow("Temps restant",&label_time);
+    f_layout.addRow("Capacité", &label_cap);
+    f_layout.addRow("Consommation",&label_conso);
+
+    v_layout.addLayout(&f_layout);
     v_layout.addWidget(&btn1);
     v_layout.addWidget(&btn2);
 }
